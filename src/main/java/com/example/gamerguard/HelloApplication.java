89ea116ea4b9aa08ce690d89407fe49123853c2c
Phoebe <phoebehoo123@gmail.com>
2024-04-23 20:30:00 +1000

    Small updates. -Phoebe

--- src/main/java/com/example/gamerguard/HelloApplication.java
+++ src/main/java/com/example/gamerguard/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("profile-settings.fxml"));
-        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.DECORATED);
         Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
 //        stage.setTitle("Welcome to GamerGuard!");
         stage.setScene(scene);
