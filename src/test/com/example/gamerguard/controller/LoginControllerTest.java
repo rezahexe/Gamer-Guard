0089b684e988c1289a56c4fbacbec6a822cdaff0
Rezah <mrmaharoof10@gmail.com>
2024-04-28 02:58:36 +1000

    Added JUnit test files for active controllers

---
+++ src/test/com/example/gamerguard/controller/LoginControllerTest.java
@@ -0,0 +1,41 @@
+package com.example.gamerguard.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LoginControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void loginButtonOnAction() {
+    }
+
+    @Test
+    void signupHyperlinkOnAction() {
+    }
+
+    @Test
+    void forgotPasswordHyperlinkOnAction() {
+    }
+
+    @Test
+    void validateLogin() {
+    }
+
+    @Test
+    void openDashboard() {
+    }
+
+    @Test
+    void cancelButtonOnAction() {
+    }
+}
---
+++ src/test/com/example/gamerguard/controller/ProfileSettingsControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.gamerguard.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProfileSettingsControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void initialize() {
+    }
+
+    @Test
+    void cancelButtonOnAction() {
+    }
+
+    @Test
+    void backButtonOnAction() {
+    }
+}
