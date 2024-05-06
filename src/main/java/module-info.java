2c304a7d68dff3860943f025d4733ca65e9b2279
Phoebe <phoebehoo123@gmail.com>
2024-05-06 22:40:04 +1000

    Simple API functionality. -Phoebe

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,6 +6,7 @@ module com.example.gamerguard {
     requires java.sql;
     requires java.desktop;
     requires java.mail;
+    requires com.fasterxml.jackson.databind;
 
 
     opens com.example.gamerguard;
